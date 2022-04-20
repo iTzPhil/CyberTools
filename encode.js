bbf29c9e4be51fda3d11784e9da149726e1619a2
iTzPhil <68789236+iTzPhil@users.noreply.github.com>
2022-04-20 12:26:39 +0200

    Answer ist now a const

--- encode.js
+++ encode.js
@@ -1,19 +1,23 @@
 const inquirer = require('inquirer')
 
 var questions = [
-  {
-    type: 'input',
-    name: 'text',
-    message: "What do you want to encode?"
-  },
-  {
-    type: 'input',
-    name: 'key',
-    message: "Which key do you want to use?"
-  }
+    {
+        type: 'input',
+        name: 'text',
+        message: "What do you want to encode?"
+    },
+    {
+        type: 'input',
+        name: 'key',
+        message: "Which key do you want to use?"
+    }
 ]
 
 inquirer.prompt(questions).then(answers => {
-  console.log(`Text: ${answers['text']}!`)
-  console.log(`Key: ${answers['key']}!`)
+    // console.log(`Text: ${answers['text']}!`)
+    // console.log(`Key: ${answers['key']}!`)
+    const text = answers['text']
+    const key = answers['key']
+    console.log(text)
+    console.log(key)
 })
