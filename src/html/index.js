0f7aee9d8c6f0a26414d40b32be9ecbcf3959860
iTzPhil <68789236+iTzPhil@users.noreply.github.com>
2023-07-31 16:55:42 +0200

    Basic UI
    Need some more Styling and color scheme but that's for another time! Completed #51

--- src/html/index.js
+++ src/html/index.js
@@ -1,22 +1,53 @@
-
 // Highlight current nav item
-var buttons = document.querySelectorAll('.nav-menu button');
+var buttons = document.querySelectorAll(".nav-menu button");
 
 buttons.forEach(function (button) {
-    if (!button.classList.contains('settings') && !button.classList.contains('current-home')) {
-        button.addEventListener('click', function () {
-            buttons.forEach(function (button) {
-                button.classList.remove('current') && button.classList.remove('current-home');
-            });
-            button.classList.add('current');
-        });
+  if (
+    !button.classList.contains("settings") &&
+    !button.classList.contains("current-home")
+  ) {
+    button.addEventListener("click", function () {
+      buttons.forEach(function (button) {
+        button.classList.remove("current") &&
+          button.classList.remove("current-home");
+      });
+      button.classList.add("current");
+    });
+  }
+  if (button.classList.contains("current-home")) {
+    button.addEventListener("click", function () {
+      buttons.forEach(function (button) {
+        button.classList.remove("current") &&
+          button.classList.remove("current-home");
+      });
+      button.classList.add("current-home");
+    });
+  }
+});
+
+// Content switcher
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let content = button.innerHTML.toLowerCase().replace(/ /g, "_");
+    let div = document.getElementById(content);
+    let divs = document.getElementById("content-area").children;
+    for (let i = 0; i < divs.length; i++) {
+      divs[i].classList.remove("active");
     }
-    if (button.classList.contains('current-home')) {
-        button.addEventListener('click', function () {
-            buttons.forEach(function (button) {
-                button.classList.remove('current') && button.classList.remove('current-home');
-            });
-            button.classList.add('current-home');
-        });
+    try {
+      div.classList.add("active");
+    } catch (error) {
+      if (error instanceof TypeError) {
+        console.log("No content found for " + content);
+      }
     }
+  });
+});
+
+// Display current password length
+const lengthInput = document.getElementById("length");
+const lengthValue = document.getElementById("length-value");
+
+lengthInput.addEventListener("input", () => {
+  lengthValue.textContent = lengthInput.value;
 });
